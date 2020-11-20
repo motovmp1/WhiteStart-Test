8bd866ce970796254e2feec158f7839d76fcc8d2
motovmp1 <vinicius.mpinho@gmail.com>
2020-11-20 12:16:23 +0000

    first commit

---
+++ WhiteStarLoginUnitTestTC0002/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WhiteStarLoginUnitTestTC0002")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WhiteStarLoginUnitTestTC0002")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("95374e19-4c38-47c1-afd0-f99e4fe36fb8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WhiteStarLoginUnitTestTC0002/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Windows;
+using System;
+using System.Threading;
+
+namespace WhiteStarLoginUnitTestTC0002
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            AppiumOptions appOptions = new AppiumOptions();
+            appOptions.AddAdditionalCapability("app", @"C:\Users\User01\source\repos\LoginaWHLoad\LoginaWHLoad\bin\Debug\LoginaWHLoad.exe");
+
+            WindowsDriver<WindowsElement> Driver = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), appOptions);
+
+            Assert.AreEqual("White Star Login Form", Driver.Title);
+            Console.WriteLine(Driver.Title);
+            Thread.Sleep(2000);
+            Driver.FindElementByAccessibilityId("txtb_User").SendKeys("Vinicius Miranda de Pinho");
+            Thread.Sleep(1000);
+            Driver.FindElementByAccessibilityId("txtb_Password").SendKeys("12345678");
+            Thread.Sleep(1000);
+            Driver.FindElementByName("LOGIN").Click();
+            Thread.Sleep(500);
+            var message = Driver.FindElementByAccessibilityId("lbl_loginsucess").Text;
+            Console.WriteLine(message);
+            Assert.AreEqual("Congratulations Sucess! ", message);
+            Thread.Sleep(3000);
+            Driver.FindElementByName("EXIT").Click();
+            
+
+        }   
+    }
+}
